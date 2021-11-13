75eb8ba51f63fc82cc3e924ad1720670dec5964d
Vitalii Harasymchuk <vitalii.harasymchuk@gmail.com>
2021-11-13 18:03:25 +0200

    first

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import userRoutes from './routes/users.js';
 import adminRoute from './routes/adminRoute.js'
 import coursesRoute from './routes/courses.js'
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
@@ -22,10 +23,10 @@ app.get('/', (req, res) => {
     res.send('Курсова');
 });
 
-const CONNECTION_URL = 'mongodb+srv://vitalii:BxW8THN1vg2T6mc4@cluster0.4r8vt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
 
